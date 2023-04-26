da6797a3164a221f347686c22229c681ec2d2697
krisbobo <kiyongbohbo@gmail.com>
2023-04-26 18:31:45 +0100

    Import header and render to app

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Offers from './pages/Offers';
 import ForgotPassword from './pages/ForgotPassword';
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
