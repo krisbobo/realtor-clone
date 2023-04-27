bb3ed5b7f44768510202cdcaa67909db71d33c77
krisbobo <kiyongbohbo@gmail.com>
2023-04-27 23:46:44 +0100

    Import uselocation to style active nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
 export default function Header() {
+  const location = useLocation();
   return (
     <>
       <header className="flex justify-between items-center px-3 mx-auto max-w-6xl sticky top-0 z-50 bg-white border-b shadow-sm">
         <img className="h-8 cursor-pointer" src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg" alt="Logo" />
         <nav>
-          <ul className="flex space-x-10 opacity-50 ">
+          <ul className="flex space-x-10 opacity-50">
             <li>
               <NavLink className="opacity-100 font-semibold text-sm py-3 border-b-[3px]" to="/">Home</NavLink>
             </li>
