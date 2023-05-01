1ef376cdf8b8beba9b6f32f8dcc392df9670417c
krisbobo <kiyongbohbo@gmail.com>
2023-05-01 23:53:06 +0100

    Update navigation links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
-import { NavLink, useLocation } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export default function Header() {
-  const location = useLocation();
   return (
     <>
-      <header className="flex justify-between items-center px-3 mx-auto max-w-6xl sticky top-0 z-50 bg-white border-b shadow-sm">
-        <img className="h-8 cursor-pointer" src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg" alt="Logo" />
+      <header className="flex justify-between items-center px-16 mx-auto sticky top-0 z-50 bg-white border-b shadow-sm">
+        <NavLink to="/">
+          <img className="h-8 cursor-pointer" src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg" alt="Logo" />
+        </NavLink>
         <nav>
           <ul className="flex space-x-10 opacity-50">
-            <li>
-              <NavLink className="opacity-100 font-semibold text-sm py-3 border-b-[3px]" to="/">Home</NavLink>
+            <li className="py-4 opacity-100 border-b-[3px] border-b-transparent">
+              <NavLink className="font-semibold py-4 text-sm text-gray-400" to="/">Home</NavLink>
             </li>
-            <li>
-              <NavLink to="/offers">Offers</NavLink>
+            <li className="py-4 opacity-100 border-b-[3px] border-b-transparent">
+              <NavLink className="font-semibold py-4 text-sm text-gray-400" to="/offers">Offers</NavLink>
             </li>
-            <li>
-              <NavLink to="/sign-in">Sign In</NavLink>
+            <li className="py-4 opacity-100 border-b-[3px] border-b-transparent">
+              <NavLink className="font-semibold py-4 text-sm text-gray-400" to="/sign-in">Sign In</NavLink>
             </li>
           </ul>
         </nav>
