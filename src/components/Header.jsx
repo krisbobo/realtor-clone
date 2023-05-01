d5a0abdbf208974f66defcaa8c147a8bae5626b8
krisbobo <kiyongbohbo@gmail.com>
2023-05-02 00:11:50 +0100

    Restyle nav links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,18 +5,16 @@ export default function Header() {
   return (
     <>
       <header className="flex justify-between items-center px-16 mx-auto sticky top-0 z-50 bg-white border-b shadow-sm">
-        <NavLink to="/">
-          <img className="h-8 cursor-pointer" src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg" alt="Logo" />
-        </NavLink>
+        <img className="h-8 cursor-pointer" src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg" alt="Logo" />
         <nav>
           <ul className="flex space-x-10 opacity-50">
-            <li className="py-4 opacity-100 border-b-[3px] border-b-transparent">
+            <li className="py-3 opacity-100 border-b-[3px] border-b-transparent">
               <NavLink className="font-semibold py-4 text-sm text-gray-400" to="/">Home</NavLink>
             </li>
-            <li className="py-4 opacity-100 border-b-[3px] border-b-transparent">
+            <li className="py-3 opacity-100 border-b-[3px] border-b-transparent">
               <NavLink className="font-semibold py-4 text-sm text-gray-400" to="/offers">Offers</NavLink>
             </li>
-            <li className="py-4 opacity-100 border-b-[3px] border-b-transparent">
+            <li className="py-3 opacity-100 border-b-[3px] border-b-transparent">
               <NavLink className="font-semibold py-4 text-sm text-gray-400" to="/sign-in">Sign In</NavLink>
             </li>
           </ul>
